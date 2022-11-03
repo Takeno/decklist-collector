82f1592bf883c852169ab05d7547be15a168f7b0
Matteo Manchi <matteo.manchi@gmail.com>
2022-11-03 10:14:37 +0100

    fix: header colors

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
 
   return (
     <header>
-      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
+      <nav className="bg-gray-900 border-gray-200 px-2 sm:px-4 py-2.5">
         <div className="container flex flex-wrap justify-between items-center mx-auto">
           <Link href="/">
             <a className="flex items-center">
@@ -24,7 +24,7 @@ export default function Header() {
                 className="mr-3 h-6 sm:h-9"
                 alt="4Seasons Summer"
               />
-              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
                 4Seasons Autumn
               </span>
             </a>
