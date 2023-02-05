093adb0747c2a7ced818c43b1f055dc5244ec3f8
Matteo Manchi <matteo.manchi@gmail.com>
2023-02-05 23:18:07 +0100

    chore: set correct channel

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,6 @@
 import {createClient} from '@supabase/supabase-js';
 
-const CHANNEL = '4seasons';
+const CHANNEL = 'law-league';
 
 const supabaseUrl = 'https://ezkbsplhdpfqdtdbgiir.supabase.co';
 const supabaseAnonKey =
