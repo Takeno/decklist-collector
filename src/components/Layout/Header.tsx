105e5f969de3afc9887610b16a8372cd7cc11482
Matteo Manchi <matteo.manchi@gmail.com>
2023-05-15 13:34:49 +0200

    impact-returns customizations

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import {useRouter} from 'next/router';
-import logo from '../../assets/logo-four-seasons.png';
+import logo from '../../assets/impact-returns.png';
 import {useUser} from '../../contexts/UserContext';
 
 export default function Header() {
@@ -19,9 +19,13 @@ export default function Header() {
         <div className="container flex flex-wrap justify-between items-center mx-auto">
           <Link href="/">
             <a className="flex items-center">
-              <img src={logo.src} className="mr-3 h-6 sm:h-9" alt="4Seasons" />
+              <img
+                src={logo.src}
+                className="mr-3 h-6 sm:h-9"
+                alt="Impact Returns"
+              />
               <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
-                4Seasons
+                Impact Returns
               </span>
             </a>
           </Link>
@@ -53,7 +57,7 @@ export default function Header() {
             )}
             <div className="" id="navbar-default">
               <a
-                href="mailto:Legacy4Seasons@gmail.com"
+                href="mailto:impactreturns@hotmail.com"
                 className="block py-2 pr-4 pl-3 text-white"
               >
                 Contact
